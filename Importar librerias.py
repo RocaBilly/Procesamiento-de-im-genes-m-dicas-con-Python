26bd050fe0410d22451ef020f0a0489392bc5ae0
RocaBilly <98423341+RocaBilly@users.noreply.github.com>
2022-01-31 14:43:24 -0600

    Add files via upload

---
+++ Importar librerias.py
@@ -0,0 +1,24 @@
+# In[]:
+
+#Aqui estan los ejemplos de la parte importar librerias    
+
+
+
+# In[]:
+
+import math 
+math.pi
+
+
+
+
+# In[]:
+    
+from math import pi
+pi
+
+# In[]:
+
+from math import sin as seno
+seno(5)
+
